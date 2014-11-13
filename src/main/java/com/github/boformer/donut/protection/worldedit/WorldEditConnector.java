1b2b342b75b72fcb2fcf410f01515ac6eb0cfa9d
boformer <felix.free@gmx.de>
2014-11-13 12:11:56 +0100

    started work on worldedit connector

---
+++ src/main/java/com/github/boformer/donut/protection/worldedit/WorldEditConnector.java
@@ -0,0 +1,16 @@
+package com.github.boformer.donut.protection.worldedit;
+
+import com.github.boformer.donut.protection.DonutProtectionPlugin;
+
+public class WorldEditConnector
+{
+	private final DonutProtectionPlugin plugin;
+
+
+	public WorldEditConnector(DonutProtectionPlugin plugin)
+	{
+		this.plugin = plugin;
+	}
+	
+	//TODO connect to worldedit if loaded
+}
