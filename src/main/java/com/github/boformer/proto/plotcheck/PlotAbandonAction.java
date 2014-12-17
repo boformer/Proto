7fc3373d2fd7fd3d1c263b1e359cd8efeabffaaf
boformer <felix.free@gmx.de>
2014-12-17 16:33:43 +0100

    package TODO

--- src/main/java/com/github/boformer/proto/plotcheck/PlotAbandonAction.java
+++ src/main/java/com/github/boformer/proto/plotcheck/PlotAbandonAction.java
@@ -3,6 +3,8 @@ package com.github.boformer.proto.plotcheck;
 //TODO javadoc
 public enum PlotAbandonAction
 {
+	//TODO move to different package?
+	
 	WORLDEDIT_SNAPSHOT_RESTORE, 
 	REGENERATE, 
 	NONE
